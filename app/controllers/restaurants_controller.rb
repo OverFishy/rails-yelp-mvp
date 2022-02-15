6d0ab5c2b74466ffa3bbfda65da00a1abeb710b5
ofek shtrit <ofek1997shtrit@gmail.com>
2022-02-15 16:27:44 +0200

    sheeeesh

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,40 @@
+class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show]
+
+  # GET /restaurants.
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/new.
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1.
+  def show
+  end
+
+  # POST /restaurants.
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Only allow a trusted parameter through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,26 @@
+require 'faker'
+
+puts 'cleaning up Data-Base'
+Restaurant.destroy_all
+puts '...'
+sleep(2)
+puts 'Data-Base is cleaned!'
+
+puts 'Creating restaurants...'
+count = 0
+20.times do
+  count += 1
+  name = Faker::Games::LeagueOfLegends.champion
+  restaurant = Restaurant.create(
+    name: "#{name}'s place",
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORIES.sample
+  )
+  puts "#{count}. #{restaurant.name} was created!"
+end
+
+puts 'All done homie'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
